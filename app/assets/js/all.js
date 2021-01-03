2f275defbb528f947b82f0cb53145c9c933938b0
WuJungHan <eva29485577@gmail.com>
2021-01-03 18:04:31 +0800

    member,ham up

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,21 +1,22 @@
-//jQuery UI Datepicker
-$(function () {
-  $("#datepicker-form").datepicker({ minDate: -20, maxDate: "+1M +10D" });
+$(document).ready(function () {
+  //jQuery UI Datepicker
+  $(function () {
+    $("#datepicker-form").datepicker({ minDate: -20, maxDate: "+1M +10D" });
+  });
+
+  $(function () {
+    $("#datepicker-to").datepicker({ minDate: -20, maxDate: "+1M +10D" });
+  });
+  //jQuery UI Datepicker end
+
+  //Swiper 左右頁
+  var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  //Swiper 結束
+
+
 });
-
-$(function () {
-  $("#datepicker-to").datepicker({ minDate: -20, maxDate: "+1M +10D" });
-});
-
-//jQuery UI Datepicker end
-
-//Swiper 左右頁
-var swiper = new Swiper('.swiper-container', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-//Swiper 結束
-
-

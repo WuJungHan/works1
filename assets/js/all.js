d831debab00039642ccc8aeee037ed4fd0c9e60c
WuJungHan <eva29485577@gmail.com>
2021-01-03 18:05:06 +0800

    Update 2021-01-03T10:05:01.686Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,24 +1,26 @@
 "use strict";
 
-//jQuery UI Datepicker
-$(function () {
-  $("#datepicker-form").datepicker({
-    minDate: -20,
-    maxDate: "+1M +10D"
+$(document).ready(function () {
+  //jQuery UI Datepicker
+  $(function () {
+    $("#datepicker-form").datepicker({
+      minDate: -20,
+      maxDate: "+1M +10D"
+    });
   });
-});
-$(function () {
-  $("#datepicker-to").datepicker({
-    minDate: -20,
-    maxDate: "+1M +10D"
-  });
-}); //jQuery UI Datepicker end
-//Swiper 左右頁
+  $(function () {
+    $("#datepicker-to").datepicker({
+      minDate: -20,
+      maxDate: "+1M +10D"
+    });
+  }); //jQuery UI Datepicker end
+  //Swiper 左右頁
 
-var swiper = new Swiper('.swiper-container', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  }
-}); //Swiper 結束
+  var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  }); //Swiper 結束
+});
 //# sourceMappingURL=all.js.map
